355d15d7db4a96edfea186424352e99951ff8129
judeomondi <judeomondi7@gmail.com>
2026-08-27 12:33:15 +0300

    2.1: Course information step 6

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,26 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    {props.parts.map(part => <Part key={part.name} part={part} />)}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p><b>total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
+
+const Course = ({course}) => {
+    return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course;
