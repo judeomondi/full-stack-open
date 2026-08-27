82900e49f701645d09ef6a560eb5750e1b863206
judeomondi <judeomondi7@gmail.com>
2026-08-27 12:34:59 +0300

    2.2: Course information step 7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -19,6 +19,9 @@ const Course = ({course}) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total
+        parts={course.parts}
+      />
     </div>
   )
 }
