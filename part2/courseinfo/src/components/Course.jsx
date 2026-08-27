cd8ec3a70a6e1613ed06547ef653164747edd4de
judeomondi <judeomondi7@gmail.com>
2026-08-27 12:49:55 +0300

    2.5: Separate module step 10

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react"
+
 const Header = (props) => <h1>{props.course}</h1>
 
 const Content = (props) => (
@@ -14,14 +16,16 @@ const Part = (props) => (
 
 const Total = (props) => <p><b>total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
     return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        parts={course.parts}
-      />
+        {courses.map(course => 
+            <Fragment key={course.id}>
+                <Header course={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts}/>
+            </Fragment>
+            )}
     </div>
   )
 }
